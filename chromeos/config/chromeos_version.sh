349f978b155411d5b513273e2da88d87c0281b82
Bernie Thompson <bhthompson@google.com>
2018-05-25 10:45:13 -0700

    Bump Chrome version after branching refs/heads/release-R68-10718.B
    Change-Id: I97f24216082e6bc09a64f8e644e15ee25a283e7c

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  CHROMEOS_BUILD=10718
+  CHROMEOS_BUILD=10719
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
@@ -45,7 +45,7 @@ else
 fi
 
 # Major version for Chrome.
-CHROME_BRANCH=68
+CHROME_BRANCH=69
 # Set CHROME values (Used for releases) to pass to chromeos-chrome-bin ebuild
 # URL to chrome archive
 CHROME_BASE=
