f3765c94b3596989f46d4590ee5fa7532afb7454
Qiyu Hu <qiyuh@google.com>
2018-10-10 11:36:06 -0700

    bluez: Add '-c' to btmon in bluetoothlog service
     - compress the output of btmon into "log.bz2"
     - remove the creation of "/run/bluetooth"
    
    BUG=chromium:872403
    TEST=start bluetoothlog
    CQ-DEPEND=CL:1274460
    Change-Id: Ic4b5057d2f1e499ca1217cf459d0cb8956b3ef43
    Reviewed-on: https://chromium-review.googlesource.com/1274166
    Commit-Ready: Qiyu Hu <qiyuh@google.com>
    Tested-by: Qiyu Hu <qiyuh@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- net-wireless/bluez/files/start_bluetoothlog.sh
+++ net-wireless/bluez/files/start_bluetoothlog.sh
@@ -3,9 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-/sbin/minijail0 -u bluetooth -g bluetooth -Gn -c0 \
-  --profile=minimalistic-mountns \
-  -- /bin/gzip < /run/bluetooth/fifo > /var/log/bluetooth/log.gz &
 exec /sbin/minijail0 -u bluetooth -g bluetooth -Nniplrvdt --uts -c2000 \
-  --profile=minimalistic-mountns -k tmpfs,/run,tmpfs,0xe -b /run/bluetooth \
-  -- /usr/bin/btmon -S0 -w /run/bluetooth/fifo
+  --profile=minimalistic-mountns \
+  -k tmpfs,/var,tmpfs,0xe -b /var/log/bluetooth,,1 \
+  -- /usr/bin/btmon -Sc0 -w /var/log/bluetooth/log.bz2
