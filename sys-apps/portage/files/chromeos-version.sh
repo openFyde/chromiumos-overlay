9324d1239d6e828ec8b7f085d92f68187f596bf2
Allen Webb <allenwebb@google.com>
2018-12-17 09:18:29 -0800

    portage: Added portage- prefix to chromeos-version.sh
    Now chromeos-version.sh can strip the portage- tag prefix for
    compatibility with the chromeos-2.3.49 branch.
    
    BUG=chromium:874977
    TEST=CQ passes with portage manifest change.
    
    Change-Id: Ie4152670b94b0c2b212cda9da3758b179e9f791d
    Reviewed-on: https://chromium-review.googlesource.com/1379076
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- sys-apps/portage/files/chromeos-version.sh
+++ sys-apps/portage/files/chromeos-version.sh
@@ -7,11 +7,13 @@
 # the package, and it prints a string on stdout with the numerical version
 # number for said repo.
 
-# This logic is similar to pym/portage/__init__.py:_LazyVersion.
+# This logic is similar to lib/portage/__init__.py:_LazyVersion.
 VERSION=$(git --git-dir="$1/.git" describe --tags)
 
 # Could be a tag like:
 #   v2.2.12-8-g15b05e76ecb4
 # or something like:
 #   cros-2.2.12-r4-1-g9b31dff37f26
-echo "${VERSION}" | sed -E -e 's:^(v|cros-)::' -e 's:-.*::'
+# or something like:
+#   portage-2.3.49-11-gb99e9b40a
+echo "${VERSION}" | sed -E -e 's:^(v|cros-|portage-)::' -e 's:-.*::'
