fd549b879a2724cf48e3471adc6415d27fa4bc86
Archie Pusaka <apusaka@chromium.org>
2019-07-09 18:52:52 +0800

    bluez: Use -l to limit bluetoothlog's btmon log size
    Previously, if btmon file size exceeds the specified limit, bluetoothlog
    will crash. This -l flag will stop btmon writing more logs once the limit
    is reached.
    
    BUG=b:136690898
    TEST=Do the following
        1) start bluetoothlog
        2) make btmon busy writing logs
        3) observe that /var/log/bluetooth/log.bz2 will not grow once it hit
           the size limit, and bluetoothlog will not crash
    
    Cq-Depend: chromium:1692246
    Change-Id: Ibe9b46de193f79d2a66c588d7aaf5f0a18b08d77
    Reviewed-on: https://chromium-review.googlesource.com/1693923
    Tested-by: Archie Pusaka <apusaka@chromium.org>
    Commit-Ready: Archie Pusaka <apusaka@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Yoni Shavit <yshavit@chromium.org>
    Reviewed-by: Archie Pusaka <apusaka@chromium.org>

--- net-wireless/bluez/files/start_bluetoothlog.sh
+++ net-wireless/bluez/files/start_bluetoothlog.sh
@@ -3,7 +3,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# Limit to 5MB of log.
+log_size_limit=5000000
+
 exec /sbin/minijail0 -u bluetooth -g bluetooth -Nniplrvdt --uts -c2000 \
   --profile=minimalistic-mountns \
   -k tmpfs,/var,tmpfs,0xe -b /var/log/bluetooth,,1 \
-  -- /usr/bin/btmon -Sc0 -w /var/log/bluetooth/log.bz2
+  -- /usr/bin/btmon -Sc0 -w /var/log/bluetooth/log.bz2 -l ${log_size_limit}
