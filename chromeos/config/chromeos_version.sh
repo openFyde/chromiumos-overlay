37a5a913b4ad13b0c765a3d4fbe9151093543d07
Bernie Thompson <bhthompson@google.com>
2019-07-30 21:01:23 -0700

    Exempt-From-Owner-Approval: Revert "Bump milestone after creating release branch release-quickfix-R75-12105.76.B."
    This reverts commit dcec3d08759243673e30b8b240d3588efac9eaf6.
    
    Reverting due to https://bugs.chromium.org/p/chromium/issues/detail?id=989337
    
    Partial revert, just the milestone, the major version was already used in a
    build so we probably want to keep it and let the master release builder uprev.
    
    BUG=chromium:989337
    TEST=None
    
    Change-Id: I3bc2c677755823045291c4b682b702790d55b0fc

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -45,7 +45,7 @@ else
 fi
 
 # Major version for Chrome.
-CHROME_BRANCH=79
+CHROME_BRANCH=78
 # Set CHROME values (Used for releases) to pass to chromeos-chrome-bin ebuild
 # URL to chrome archive
 CHROME_BASE=
