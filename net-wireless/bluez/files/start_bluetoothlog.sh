7d1f9d20467c777ba06650f94d91b1455f25c501
Archie Pusaka <apusaka@chromium.org>
2020-01-06 13:03:10 +0800

    bluez: Use -R flag to rotate bluetoothlog's btmon logs
    Previously, when btmon log size exceeds the limit, we will stop logging
    and lose all the valuable information. Therefore, the -R rotate flag is
    introduced to rotate two logs. This way, we will keep the most recent
    logs, although at the expense of losing the earlier, probably less
    important logs.
    
    BUG=b:143118775,b:146524142
    TEST=Do the following
        1) start bluetoothlog (can be done by logging with corp account)
        2) make btmon busy writing logs
        3) observe that once /var/log/bluetooth/log.bz2 hits the size limit,
           it will be renamed to log.bz2.old, and an empty new log.bz2 is
           created as the place where subsequent logs will be written into.
    
    Change-Id: Ia0df787da6076bd7653e724cfb3b2e60e763618d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/1980386
    Tested-by: Archie Pusaka <apusaka@chromium.org>
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Commit-Queue: Archie Pusaka <apusaka@chromium.org>

--- net-wireless/bluez/files/start_bluetoothlog.sh
+++ net-wireless/bluez/files/start_bluetoothlog.sh
@@ -3,10 +3,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Limit to 5MB of log.
-log_size_limit=5000000
+# Limit each of the 2 rotating logs to 2.5MB.
+log_size_limit=2500000
 
 exec /sbin/minijail0 -u bluetooth -g bluetooth -Nniplrvdt --uts -c2000 \
   --profile=minimalistic-mountns \
   -k tmpfs,/var,tmpfs,0xe -b /var/log/bluetooth,,1 \
-  -- /usr/bin/btmon -Sc0 -w /var/log/bluetooth/log.bz2 -l ${log_size_limit}
+  -- /usr/bin/btmon -Sc0R -w /var/log/bluetooth/log.bz2 -l "${log_size_limit}"
