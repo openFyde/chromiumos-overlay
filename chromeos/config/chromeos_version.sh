5e4a1965c963b9d818691e96fd775a22c32c8b28
YH Lin <yueherngl@google.com>
2020-01-09 15:39:46 -0800

    Bump build number for source branch after creating branch factory-excelsior-12812.B
    Change-Id: I243a74052d91b615e3ccb1107d32aab7271f02fe

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  CHROMEOS_BUILD=12817
+  CHROMEOS_BUILD=12818
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
