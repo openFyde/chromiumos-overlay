4ca1cf7dff02cb312f73a474691823a6d8d740ea
Andrew Lamb <andrewlamb@chromium.org>
2020-01-14 13:37:30 -0700

    chromiumos-overlay: Add skeleton of DesignConfig.
    - Right now just includes bluetooth_config, as an
    example.
    
    - Want to get consensus on names, location,
    generation method, etc.
    
    BUG=chromium:1040976
    TEST=generate.sh
    
    Cq-Depend: chromium:1999138
    Change-Id: I2cf3870d838f0ca4cdb666c266d95e04bf3769a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2001472
    Reviewed-by: C Shapiro <shapiroc@chromium.org>
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>

---
+++ proto/generate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+#
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+protoc -I. -I../../../.. design_config.proto --python_out=proto_bindings
