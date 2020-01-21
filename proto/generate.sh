75064f4d0be524ae2b5d8cd8765f080289ade907
Andrew Lamb <andrewlamb@chromium.org>
2020-01-21 14:22:15 -0700

    Add DesignVariantIdScanConfig to DesignVariantConfig.
    - Rename DesignConfig -> DesignVariantConfig for
    consistency.
    
    BUG=chromium:1040976
    TEST=./generate.sh
    
    Change-Id: I3b37cf1b7cd8d827535b153440c8b73afdbe357f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2013571
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: C Shapiro <shapiroc@chromium.org>

--- proto/generate.sh
+++ proto/generate.sh
@@ -4,4 +4,4 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-protoc -I. -I../../../.. design_config.proto --python_out=proto_bindings
+protoc -I. -I../../../.. design_variant_config.proto --python_out=proto_bindings
