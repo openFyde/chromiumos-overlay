295a8a5672d48ecb136edf9d764f95af5422a0bb
Bernie Thompson <bhthompson@google.com>
2020-01-27 17:06:23 -0800

    Bump build number for source branch after creating branch stabilize-12859.B
    Change-Id: Ic43dd2347f984c97f4043b84be99af9da856d291

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  CHROMEOS_BUILD=12862
+  CHROMEOS_BUILD=12863
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
