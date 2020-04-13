e088975deedd828a96c2e9ee1d06a827db7dc781
Howard Chung <howardchung@google.com>
2020-04-13 16:01:53 +0800

    bluez: update btmon file rotation option flag
    Replace btmon option from '-R --rotate' to '-f --file-rotate'.
    
    BUG=b:153834146
    TEST=run build_package successfully
    
    Cq-Depend: 2145290
    Change-Id: I7bce1334eec688dd57a4b8108e49a49097e9caab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2147123
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Archie Pusaka <apusaka@chromium.org>
    Tested-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Commit-Queue: Yun-Hao Chung <howardchung@google.com>

--- net-wireless/bluez/files/start_bluetoothlog.sh
+++ net-wireless/bluez/files/start_bluetoothlog.sh
@@ -9,4 +9,4 @@ log_size_limit=2500000
 exec /sbin/minijail0 -u bluetooth -g bluetooth -Nniplrvdt --uts -c2000 \
   --profile=minimalistic-mountns \
   -k tmpfs,/var,tmpfs,0xe -b /var/log/bluetooth,,1 \
-  -- /usr/bin/btmon -Sc0R -w /var/log/bluetooth/log.bz2 -l "${log_size_limit}"
+  -- /usr/bin/btmon -Sc0f -w /var/log/bluetooth/log.bz2 -l "${log_size_limit}"
