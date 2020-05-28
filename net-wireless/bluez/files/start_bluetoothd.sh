a916bf987dfba20d46791f123372011a8caab07f
Sonny Sasaka <sonnysasaka@chromium.org>
2020-05-27 17:04:19 -0700

    bluez: Do not specify per-board config file
    As DeviceID will be changed to be uniform across boards, and main.conf
    contains only DeviceID, the per-board main.conf is no longer needed.
    This patch changes the BlueZ start script to always load
    /etc/bluetooth/main.conf.
    
    BUG=b:152526402
    TEST=restart bluetoothd and check that DeviceID is as hardcoded in
    CL:2219367.
    
    Change-Id: Iae8f66490366caa92eff45d064c472aacf180bc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2219216
    Tested-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Alain Michaud <alainm@chromium.org>

--- net-wireless/bluez/files/start_bluetoothd.sh
+++ net-wireless/bluez/files/start_bluetoothd.sh
@@ -8,32 +8,10 @@
 # the legacy board-specific configuration (main.conf) if the config file
 # is present.
 
-config_file_param=""
 bluetooth_dir="/etc/bluetooth"
-model_dir="${bluetooth_dir}/models"
-legacy_conf_file="${bluetooth_dir}/main.conf"
+conf_file="${bluetooth_dir}/main.conf"
+config_file_param="--configfile=${conf_file}"
 
-# TODO(shapiroc): Remove once all config migrated to cros_config
-if [ -d ${model_dir} ]; then
-  # Note: the below usage of the model name is not a good pattern, but should
-  # apply to non-unibuild only as the below bluetooth config will override it.
-  model="$(cros_config / name)"
-  model_conf_file="${model_dir}/${model}.conf"
-  if [ -e ${model_conf_file} ]; then
-    config_file_param="--configfile=${model_conf_file}"
-  fi
-fi
-
-device_config_file="$(cros_config /bluetooth/config system-path)"
-if [ -z "${config_file_param}" ] && [ -e "${device_config_file}" ]; then
-  config_file_param="--configfile=${device_config_file}"
-fi
-
-# If the model specific configuration is not present, check for the
-# legacy board-specific configuration
-if [ -z "${config_file_param}" ] && [ -e "${legacy_conf_file}" ]; then
-  config_file_param="--configfile=${legacy_conf_file}"
-fi
 exec /sbin/minijail0 -u bluetooth -g bluetooth -G \
   -c 3500 -n -- \
   /usr/libexec/bluetooth/bluetoothd ${BLUETOOTH_DAEMON_OPTION} --nodetach \
