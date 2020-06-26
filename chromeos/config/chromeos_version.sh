b5fcd8f92f253103672386190aa321b2b782cf01
Daniel Gagnon <dgagnon@google.com>
2020-06-26 09:49:28 -0700

    Bump milestone after creating release branch release-R85-13310.B

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -45,7 +45,7 @@ else
 fi
 
 # Major version for Chrome.
-CHROME_BRANCH=85
+CHROME_BRANCH=86
 # Set CHROME values (Used for releases) to pass to chromeos-chrome-bin ebuild
 # URL to chrome archive
 CHROME_BASE=
