92d6dd2121d00879acce0ba3e5ae0fbebb513857
Sonny Sasaka <sonnysasaka@chromium.org>
2020-06-26 15:17:01 -0700

    bluez: Automatically set DeviceID based on Chrome OS version
    This uses /etc/os-release at runtime to detect the current Chrome OS
    version number and replaces the DeviceID config based on that.
    
    BUG=b:152526402
    TEST=deploy bluez package, restart bluetoothd and check the used
    DeviceID from the debug logs in /var/log/messages
    
    Change-Id: If52e89b7f7443962a50fe59c4f02a02d4a53735a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2270916
    Reviewed-by: Alain Michaud <alainm@chromium.org>
    Reviewed-by: Stephane Belmon <sbelmon@google.com>
    Commit-Queue: Alain Michaud <alainm@chromium.org>
    Tested-by: Alain Michaud <alainm@chromium.org>

--- net-wireless/bluez/files/start_bluetoothd.sh
+++ net-wireless/bluez/files/start_bluetoothd.sh
@@ -10,7 +10,19 @@
 
 bluetooth_dir="/etc/bluetooth"
 conf_file="${bluetooth_dir}/main.conf"
-config_file_param="--configfile=${conf_file}"
+
+# Make a copy of main.conf to /var to make it editable
+var_conf_file="/var/lib/bluetooth/main.conf"
+cp "${conf_file}" "${var_conf_file}"
+# For security, limit the file permissions to only user "bluetooth".
+chown bluetooth: "${var_conf_file}"
+chmod 0600 "${var_conf_file}"
+# Set the DeviceID based on Chrome OS version.
+os_version="$(awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release)"
+hex_os_version="$(printf '%04x' "${os_version}")"
+sed -i -E "s/(bluetooth:00e0:c405:)0000/\1${hex_os_version}/" "${var_conf_file}"
+
+config_file_param="--configfile=${var_conf_file}"
 
 exec /sbin/minijail0 -u bluetooth -g bluetooth -G \
   -c 3500 -n -- \
