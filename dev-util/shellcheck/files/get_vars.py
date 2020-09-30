2236d7ff17cde6d91c7fb85713db43464fc03a1d
Mason Wilde <masonwilde@google.com>
2020-09-30 12:31:19 -0600

    ShellCheck: Remove generic vars from get_vars
    These variables are now handled in the manual vars section of the
    Data.hs file
    
    BUG=chromium:1133782
    TEST=Ran the tool and observed output
    
    Change-Id: I0ca80237611a16ef31e4495e3c0c2ed437df01a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2441033
    Tested-by: Mason Wilde <masonwilde@google.com>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Mason Wilde <masonwilde@google.com>

--- dev-util/shellcheck/files/get_vars.py
+++ dev-util/shellcheck/files/get_vars.py
@@ -3,7 +3,6 @@
 # Copyright 2019 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Extract eclass variable names into Haskell list format."""
 
 from __future__ import print_function
@@ -16,27 +15,8 @@ import textwrap
 # Variables that have been seen in any eclass previously.
 SEEN_VARS = set()
 
-
 # Matches a line that declares a variable in an eclass.
 VAR_RE = re.compile(r'@(?:ECLASS-)?VARIABLE:\s*(\w+)$')
-
-
-# Variables from portage instead of a specific eclass.
-GENERIC_VARS = (
-    'A', 'BDEPEND', 'BROOT', 'CATEGORY', 'D', 'DEFINED_PHASES', 'DEPEND',
-    'DESCRIPTION', 'DISTDIR', 'DOCS', 'EAPI', 'ECLASS', 'ED', 'EPREFIX',
-    'EROOT', 'ESYSROOT', 'EXTRA_ECONF', 'EXTRA_EINSTALL', 'EXTRA_MAKE',
-    'FEATURES', 'FILESDIR', 'HOME', 'HOMEPAGE', 'HTML_DOCS', 'INHERITED',
-    'IUSE', 'KEYWORDS', 'LICENSE', 'P', 'PATCHES', 'PDEPEND', 'PF',
-    'PKG_INSTALL_MASK', 'PKGUSE', 'PN', 'PR', 'PROPERTIES', 'PROVIDES_EXCLUDE',
-    'PV', 'PVR', 'QA_AM_MAINTAINER_MODE', 'QA_CONFIGURE_OPTIONS',
-    'QA_DESKTOP_FILE', 'QA_DT_NEEDED', 'QA_EXECSTACK', 'QA_FLAGS_IGNORED',
-    'QA_MULTILIB_PATHS', 'QA_PREBUILT', 'QA_PRESTRIPPED', 'QA_SONAME',
-    'QA_SONAME_NO_SYMLINK', 'QA_TEXTRELS', 'QA_WX_LOAD', 'RDEPEND',
-    'REPOSITORY', 'REQUIRED_USE', 'REQUIRES_EXCLUDE', 'RESTRICT', 'ROOT', 'S',
-    'SLOT', 'SRC_TEST', 'SRC_URI', 'STRIP_MASK', 'SUBSLOT', 'SYSROOT', 'T',
-    'WORKDIR',
-)
 
 
 def print_var_list(eclass, eclass_vars):
@@ -69,10 +49,6 @@ def process_file(eclass_path):
 
 
 def main(argv):
-  SEEN_VARS.update(GENERIC_VARS)
-
-  print_var_list('generic ebuilds', GENERIC_VARS)
-
   for path in sorted(argv, key=os.path.basename):
     if not path.endswith('.eclass'):
       continue
