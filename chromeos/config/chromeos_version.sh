bef629c66327deb0fdbba1781f295c40d7abc922
Cindy Bayless <cindyb@google.com>
2020-10-06 13:23:26 -0600

    Bump CHROMEOS_BUILD number for source branch master after creating branch stabilize-rust-13514.B

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  CHROMEOS_BUILD=13515
+  CHROMEOS_BUILD=13516
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
