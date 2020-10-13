23405fb479b91085a834916d9e6debd1f0515536
Fletcher Woodruff <fletcherw@chromium.org>
2020-10-13 10:07:03 -0600

    sane-airscan: further reduce fuzzer input size
    We're still getting fuzzer timeouts with a 256kb limit. Reduce it again
    to 32kb.
    
    BUG=chromium:1134991
    TEST=none
    
    Change-Id: Ibdf598d04bcb99f0bc0900b3fd32d0f45d482145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2467451
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Tested-by: Fletcher Woodruff <fletcherw@chromium.org>
    Commit-Queue: Fletcher Woodruff <fletcherw@chromium.org>

--- media-gfx/sane-airscan/files/airscan_xml_fuzzer.cc
+++ media-gfx/sane-airscan/files/airscan_xml_fuzzer.cc
@@ -16,10 +16,10 @@ void noerrs(void *ctx, const char*msg, ...) {
   // Ignore the libxml error messages.
 }
 
-constexpr int kMaxInputSize = 256 * 1024;
+constexpr int kMaxInputSize = 32 * 1024;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  // Limit fuzzer input size to 256KB.  libxml can take a very long time to
+  // Limit fuzzer input size to 32KB.  libxml can take a very long time to
   // parse very large inputs, which causes the fuzzer to time out.
   if (size > kMaxInputSize) {
     return 0;
