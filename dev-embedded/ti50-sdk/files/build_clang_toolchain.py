20b3ee4aac1c63debc42274cffbb1aac98e43925
George Burgess IV <gbiv@google.com>
2021-03-03 13:32:43 -0800

    ti50-sdk: initial import
    This imports Ti50's SDK into CrOS, trying as much as possible to keep
    with the scripts/setup that Ti50 has been running with so far.
    
    Ti50's SDK is ~2GB, and occupies /opt/ti50-sdk. It contains a mixture of
    clang, newlib, and rust. The design of this was discussed here:
    https://docs.google.com/document/d/1Fs_oRvanPtEPBia1p88224FVt0Jt1iAr0CDICW_qrY8/edit
    
    It's hoped that, in the future, we'll integrate this all with Chrome OS'
    primary C/C++/Rust toolchains. Given the deadlines of various parties,
    that doesn't seem feasible to do in our first pass.
    
    BUG=chromium:1179917
    TEST=`sudo emerge ti50-sdk`; ti50 builds with a few internal patches.
    
    Change-Id: I8c3e9fa5011b447cc0025b9c0b1ff963bdaad805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2792884
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

---
+++ dev-embedded/ti50-sdk/files/build_clang_toolchain.py
@@ -0,0 +1,387 @@
+#!/usr/bin/env python3
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Builds the clang toolchain for Ti50."""
+
+# pylint: disable=cros-logging-import
+
+import argparse
+import logging
+import os
+from pathlib import Path
+import shlex
+import shutil
+import subprocess
+import sys
+from typing import List
+
+# CFlags used when building RISCV runtime libraries.
+RISCV_RUNTIME_CFLAGS = (
+    '-O2',
+    '--target=riscv32-unknown-elf',
+    '-DVISIBILITY_HIDDEN',
+    '-DNDEBUG',
+    '-fno-builtin',
+    '-fvisibility=hidden',
+    '-fomit-frame-pointer',
+    '-mno-relax',
+    '-fforce-enable-int128',
+    '-DCRT_HAS_INITFINI_ARRAY',
+    '-march=rv32imcxsoteria',
+    '-ffunction-sections',
+    '-fdata-sections',
+    '-fstack-size-section',
+    '-mcmodel=medlow',
+    '-Wno-unused-command-line-argument',
+)
+
+# Flags passed to cmake command for building LLVM
+# TODO(sukhomlinov): These args are copy-pasted; they might not all be
+# necessary.
+CMAKE_FLAGS = (
+    '-G',
+    'Ninja',
+    '-DCMAKE_BUILD_TYPE=Release',
+    '-DLLVM_ENABLE_LIBCXX=ON',
+    '-DLLVM_OPTIMIZED_TABLEGEN=ON',
+    '-DLLVM_BUILD_TESTS=OFF',
+    '-DCLANG_ENABLE_STATIC_ANALYZER=ON',
+    '-DCLANG_DEFAULT_RTLIB="compiler-rt"',
+    '-DLLVM_DEFAULT_TARGET_TRIPLE=riscv32-unknown-elf',
+    '-DLLVM_INSTALL_BINUTILS_SYMLINKS=ON',
+    '-DLLVM_INSTALL_CCTOOLS_SYMLINKS=ON',
+    '-DCLANG_DEFAULT_LINKER=ld.lld',
+    '-DLLVM_ENABLE_BACKTRACES=OFF',
+    '-DLLVM_INCLUDE_EXAMPLES=OFF',
+    '-DLLVM_DYLIB_COMPONENTS=""',
+    '-DLLVM_LINK_LLVM_DYLIB=OFF',
+    '-DLLVM_BUILD_STATIC=OFF',
+    '-DLLVM_INSTALL_UTILS=ON',
+    '-DLLVM_ENABLE_Z3_SOLVER=OFF',
+    '-DLLVM_USE_RELATIVE_PATHS_IN_FILES=ON',
+    '-DLLVM_TARGETS_TO_BUILD=RISCV;X86',
+    '-DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra;lldb;lld;polly',
+)
+
+# Flags for configuring newlib.
+NEWLIB_CONFIG_FLAGS = (
+    '--target=riscv32-unknown-elf',
+    '--enable-newlib-reent-small',
+    '--disable-newlib-fvwrite-in-streamio',
+    '--disable-newlib-fseek-optimization',
+    '--disable-newlib-wide-orient',
+    '--enable-newlib-nano-malloc',
+    '--disable-newlib-unbuf-stream-opt',
+    '--enable-lite-exit',
+    '--enable-newlib-global-atexit',
+    '--enable-newlib-nano-formatted-io',
+    '--disable-newlib-supplied-syscalls',
+    '--disable-nls',
+    '--enable-newlib-io-c99-formats',
+    '--enable-newlib-io-long-long',
+)
+
+
+def remove_path_if_exists(file_or_dir: Path):
+  """Removes the given file/directory/symlink if it exists."""
+  if file_or_dir.is_file() or file_or_dir.is_symlink():
+    file_or_dir.unlink()
+  elif file_or_dir.is_dir():
+    shutil.rmtree(file_or_dir)
+  else:
+    assert not file_or_dir.exists(), file_or_dir
+
+
+def find_clang_resource_dir(llvm_install_dir: Path) -> Path:
+  """Locates the resource directory of the clang in |llvm_install_dir|."""
+  return Path(
+      subprocess.check_output(
+          [str(llvm_install_dir / 'bin' / 'clang'), '-print-resource-dir'],
+          encoding='utf-8',
+      ).strip())
+
+
+def build_llvm(llvm_dir: Path, build_dir: Path, install_dir: Path):
+  """Builds and installs LLVM to install_dir, plus crt{begin,end} for RISCV."""
+  build_dir.mkdir(parents=True, exist_ok=True)
+
+  if not (build_dir / 'build.ninja').exists():
+    logging.info('Configuring LLVM')
+    cmd = [
+        'cmake',
+        f'-DCMAKE_INSTALL_PREFIX={install_dir}',
+        f'-DDEFAULT_SYSROOT={install_dir}',
+        f'-DGCC_INSTALL_PREFIX={install_dir}',
+        str(llvm_dir / 'llvm'),
+    ]
+    cmd += CMAKE_FLAGS
+    subprocess.check_call(cmd, cwd=build_dir)
+
+  logging.info('Building + installing LLVM')
+  subprocess.check_call(['ninja', 'install'], cwd=build_dir)
+
+  # And now we build the runtime library. Whee.
+  clang_rtlib_prefix = [
+      str(install_dir / 'bin' / 'clang'),
+      '-c',
+      '-v',
+  ]
+  clang_rtlib_prefix += RISCV_RUNTIME_CFLAGS
+
+  libdir = find_clang_resource_dir(install_dir)
+  (libdir / 'lib').mkdir(parents=True, exist_ok=True)
+  subprocess.check_call(clang_rtlib_prefix + [
+      str(llvm_dir / 'compiler-rt' / 'lib' / 'crt' / 'crtbegin.c'),
+      '-o',
+      str(libdir / 'lib' / 'clang_rt.crtbegin-riscv32.o'),
+  ])
+
+  subprocess.check_call(clang_rtlib_prefix + [
+      str(llvm_dir / 'compiler-rt' / 'lib' / 'crt' / 'crtend.c'),
+      '-o',
+      str(libdir / 'lib' / 'clang_rt.crtend-riscv32.o'),
+  ])
+
+
+def build_newlib(rv_clang_bin: Path, newlib_dir: Path, newlib_build_dir: Path,
+                 install_dir: Path):
+  """Builds and installs newlib into |install_dir|."""
+  newlib_build_dir.mkdir(parents=True)
+
+  logging.info("Setting up newlib's build")
+
+  env_with_rv_clang = os.environ.copy()
+  # ...I don't *want* to have all of this in the environment, but if all of
+  # this (ignoring PATH) is provided to |./configure| as args, |make| gets
+  # angry that it doesn't see the same values in its environment when invoked.
+  #
+  # TODO(sukhomlinov): This seems like brokenness in newlib, maybe?
+  env_with_rv_clang.update({
+      'LDFLAGS': f'{env_with_rv_clang.get("LDFLAGS", "")} -fuse-ld=lld',
+      'PATH': f'{rv_clang_bin}:{env_with_rv_clang["PATH"]}',
+  })
+
+  tool_substitutions = (
+      ('AR', 'llvm-ar'),
+      ('AS', 'llvm-as'),
+      ('NM', 'llvm-nm'),
+      ('OBJDUMP', 'llvm-objdump'),
+      ('RANLIB', 'llvm-ranlib'),
+  )
+
+  for var_name, executable in tool_substitutions:
+    executable_path = install_dir / 'bin' / executable
+    env_with_rv_clang.update({
+        var_name: executable_path,
+        f'{var_name}_FOR_TARGET': executable_path,
+    })
+
+  env_with_rv_clang.update({
+      f'{var_name}_FOR_TARGET': install_dir / 'bin' / exe
+      for var_name, exe in (('CC', 'clang'), ('CXX', 'clang++'))
+  })
+
+  cmd = [
+      './configure',
+      f'--prefix={newlib_build_dir}',
+      f'CFLAGS_FOR_TARGET={" ".join(RISCV_RUNTIME_CFLAGS)}',
+      f'CXXFLAGS_FOR_TARGET={" ".join(RISCV_RUNTIME_CFLAGS)}',
+      'LDFLAGS=-fuse-ld=lld',
+      'LDFLAGS_FOR_TARGET=-fuse-ld=lld',
+  ]
+  cmd += NEWLIB_CONFIG_FLAGS
+  subprocess.check_call(cmd, cwd=newlib_dir, env=env_with_rv_clang)
+
+  logging.info('Building newlib')
+
+  jobs = f'-j{os.cpu_count()}'
+  subprocess.check_call(['make', jobs], cwd=newlib_dir, env=env_with_rv_clang)
+  subprocess.check_call(['make', jobs, 'install'],
+                        cwd=newlib_dir,
+                        env=env_with_rv_clang)
+
+  logging.info("Fixing newlib's installation")
+  # Quoting the script that I'm basing this off of:
+  # """
+  # move includes/libs to where they can be found by default
+  # this is also a hack, but avoid adding paths explicitly
+  # This is because riscv32-unknown-elf target only use
+  # $INSTALL_DIR/lib and $INSTALL_DIR/lib/clang/11.0.0/lib paths,
+  # ignoring --libdir and --sysroot for cmake configure
+  # $INSTALL_DIR/include contains llvm headers not used for C compilation
+  # normally, unless you build llvm tools, but directory is in default
+  # search path, so use it for libc headers.
+  # """
+  installed_include_dir = install_dir / 'include'
+  old_include_dir = install_dir / 'include.old'
+  if installed_include_dir.exists():
+    shutil.move(installed_include_dir, old_include_dir)
+
+  shutil.copytree(newlib_build_dir / 'riscv32-unknown-elf' / 'include',
+                  installed_include_dir)
+
+  if old_include_dir.exists():
+    shutil.rmtree(old_include_dir)
+  for f in (install_dir / 'lib').glob('*.a'):
+    f.unlink()
+
+  lib_base = install_dir / 'lib'
+  for f in (newlib_build_dir / 'riscv32-unknown-elf' / 'lib').iterdir():
+    shutil.copy2(f, lib_base / f.name)
+
+
+def build_compiler_rt(llvm_path: Path, compiler_rt_build_dir: Path,
+                      install_dir: Path):
+  """Builds compiler_rt and installs artifacts in |install_dir|."""
+  compiler_rt_build_dir.mkdir(parents=True)
+  run_clang = [str(install_dir / 'bin' / 'clang'), '-c']
+  run_clang += RISCV_RUNTIME_CFLAGS
+  compiler_rt_builtins = llvm_path / 'compiler-rt' / 'lib' / 'builtins'
+  subprocess.check_call(
+      run_clang + [
+          str(compiler_rt_builtins / 'riscv' / 'mulsi3.S'),
+      ],
+      cwd=compiler_rt_build_dir,
+  )
+
+  # It's expected that some of these builds fail for various reasons (no atomic
+  # uint64_t on riscv, etc.)
+  expected_failures = {
+      'atomic_flag_clear.c',
+      'atomic_flag_clear_explicit.c',
+      'atomic_flag_test_and_set.c',
+      'atomic_flag_test_and_set_explicit.c',
+      'atomic_signal_fence.c',
+      'atomic_thread_fence.c',
+      'emutls.c',
+      'enable_execute_stack.c',
+  }
+
+  actual_failures = set()
+  for f in compiler_rt_builtins.glob('*.c'):
+    # Build the expected failures, since doing so is quick, and if they _do_
+    # successfully build, that sounds like smoke to me.
+    command = run_clang + [str(f)]
+    suffix = ' (expected to fail)' if f.name in expected_failures else ''
+    print(f'Running {" ".join(shlex.quote(x) for x in command)}{suffix}...')
+    failed = subprocess.call(command, cwd=compiler_rt_build_dir)
+
+    if failed:
+      actual_failures.add(f.name)
+
+  if actual_failures != expected_failures:
+    expected_failures = sorted(expected_failures)
+    actual_failures = sorted(actual_failures)
+    raise ValueError('Mismatch between expected failed-to-build files '
+                     f'({expected_failures}) and actual ({actual_failures})')
+
+  libdir = find_clang_resource_dir(install_dir)
+  link_command = [
+      str(install_dir / 'bin' / 'llvm-ar'),
+      'rc',
+      str(libdir / 'libclang_rt.builtins-riscv32.a'),
+  ]
+  link_command += (str(x) for x in compiler_rt_build_dir.glob('*.o'))
+  subprocess.check_call(link_command)
+
+
+def log_install_paths(install_dir: Path):
+  """Prints install paths to stdout."""
+  clang = str(install_dir / 'bin' / 'clang')
+  subprocess.check_call([clang, '-E', '-x', 'c++', '/dev/null', '-v'])
+  subprocess.check_call([clang, '-print-search-dirs'])
+
+
+def get_parser():
+  """Creates a parser for commandline args."""
+  parser = argparse.ArgumentParser(
+      description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser.add_argument(
+      '--work-dir',
+      required=True,
+      type=Path,
+      help='Path to put build artifacts in.',
+  )
+  parser.add_argument(
+      '--llvm-dir',
+      required=True,
+      type=Path,
+      help='Path to the LLVM checkout.',
+  )
+  parser.add_argument(
+      '--newlib-dir',
+      required=True,
+      type=Path,
+      help='Path to the newlib checkout.',
+  )
+  parser.add_argument(
+      '--no-clean-llvm',
+      action='store_false',
+      dest='clean_llvm',
+      help="Don't wipe out LLVM's build directory if it exists.",
+  )
+  parser.add_argument(
+      '--no-clean-newlib',
+      action='store_false',
+      dest='clean_newlib',
+      help="Don't wipe out newlib's build directory if it exists.",
+  )
+  parser.add_argument(
+      '--install-dir',
+      required=True,
+      type=Path,
+      help='Path to the place to install artifacts.',
+  )
+  return parser
+
+
+def main(argv: List[str]):
+  logging.basicConfig(
+      format='%(asctime)s: %(levelname)s: %(filename)s:%(lineno)d: %(message)s',
+      level=logging.INFO,
+  )
+
+  opts = get_parser().parse_args(argv)
+  work_dir = opts.work_dir
+  install_dir = opts.install_dir.resolve()
+  llvm_path = opts.llvm_dir.resolve()
+  newlib_path = opts.newlib_dir.resolve()
+
+  work_dir.mkdir(parents=True, exist_ok=True)
+
+  remove_path_if_exists(install_dir)
+  install_dir.mkdir(parents=True)
+
+  llvm_build_dir = work_dir / 'llvm-build' / 'Release'
+  if opts.clean_llvm:
+    remove_path_if_exists(llvm_build_dir)
+  build_llvm(llvm_path, llvm_build_dir, install_dir)
+
+  newlib_build_dir = work_dir / 'newlib' / 'build'
+  if opts.clean_newlib:
+    # Ugly: newlib builds to _both_ the build dir and an in-git-directory dir;
+    # would be nice if it only used one.
+    remove_path_if_exists(newlib_build_dir)
+    # In an |emerge| flow, we don't need this, but repeated |ebuild|s could
+    # leave us with a dirty source directory.
+    if (newlib_path / 'Makefile').exists():
+      remove_path_if_exists(newlib_path / 'riscv32-unknown-elf')
+      subprocess.check_call(['make', 'distclean'], cwd=newlib_path)
+
+  build_newlib(
+      rv_clang_bin=install_dir / 'bin',
+      newlib_dir=newlib_path,
+      newlib_build_dir=newlib_build_dir,
+      install_dir=install_dir,
+  )
+
+  compiler_rt_build_dir = work_dir / 'compiler-rt-build'
+  remove_path_if_exists(compiler_rt_build_dir)
+  build_compiler_rt(llvm_path, compiler_rt_build_dir, install_dir)
+
+  log_install_paths(install_dir)
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
---
+++ dev-embedded/ti50-sdk/ti50-sdk-0.0.1.ebuild
@@ -0,0 +1,143 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+EAPI=7
+
+PYTHON_COMPAT=( python3_{6..9} )
+
+inherit cros-constants python-any-r1 toolchain-funcs
+
+DESCRIPTION="Ebuild that installs Ti50's SDK"
+LICENSE="|| ( MIT Apache-2.0 ) BSD-1 BSD-2 BSD-4 UoI-NCSA"
+KEYWORDS="*"
+SLOT="0"
+
+# The llvm src tarball was manually packed from a checkout of
+# https://github.com/llvm/llvm-project at ${LLVM_SHA}, using
+# ${FILESDIR}/pack_git_tarball.py.
+LLVM_SHA="bb852a09ae36"
+LLVM_SRC_TARBALL_NAME="llvm-${LLVM_SHA}-src"
+SRC_URI="https://commondatastorage.googleapis.com/chromeos-localmirror/distfiles/${LLVM_SRC_TARBALL_NAME}.tar.xz"
+
+BOOTSTRAP_HOST_TRIPLE="x86_64-unknown-linux-gnu"
+
+RUST_STAGE0_DATE="2020-08-26"
+RUST_VERSION="0da58007451a"
+
+RUST_PREFIX="rust-${RUST_VERSION}"
+RUST_SRC_TARBALL_NAME="rustc-${RUST_VERSION}-src"
+RUST_CARGO_TARBALL_NAME="cargo-beta-${BOOTSTRAP_HOST_TRIPLE}"
+RUST_STAGE0_TARBALL_NAME="rustc-beta-${BOOTSTRAP_HOST_TRIPLE}"
+RUST_STD_TARBALL_NAME="rust-std-beta-${BOOTSTRAP_HOST_TRIPLE}"
+RUST_RUSTFMT_TARBALL_NAME="rustfmt-beta-${BOOTSTRAP_HOST_TRIPLE}"
+# The rust src tarball was manually packed from a checkout of
+# https://github.com/rust-lang/rust at ${RUST_VERSION}, using
+# ${FILESDIR}/pack_git_tarball.py with |--post-copy-command 'cargo vendor'|.
+#
+# NOTE that this also had
+# rust-lang/rust@e715c7f234ba25c25b98894c822de9e7cf87558c applied; without
+# that, nothing `dist`s except for rust-std.
+SRC_URI+="
+	https://commondatastorage.googleapis.com/chromeos-localmirror/distfiles/${RUST_PREFIX}-${RUST_SRC_TARBALL_NAME}.tar.xz
+	https://static.rust-lang.org/dist/${RUST_STAGE0_DATE}/${RUST_CARGO_TARBALL_NAME}.tar.xz -> ${RUST_PREFIX}-${RUST_CARGO_TARBALL_NAME}.tar.xz
+	https://static.rust-lang.org/dist/${RUST_STAGE0_DATE}/${RUST_STAGE0_TARBALL_NAME}.tar.xz -> ${RUST_PREFIX}-${RUST_STAGE0_TARBALL_NAME}.tar.xz
+	https://static.rust-lang.org/dist/${RUST_STAGE0_DATE}/${RUST_STD_TARBALL_NAME}.tar.xz -> ${RUST_PREFIX}-${RUST_STD_TARBALL_NAME}.tar.xz
+	https://static.rust-lang.org/dist/${RUST_STAGE0_DATE}/${RUST_RUSTFMT_TARBALL_NAME}.tar.xz -> ${RUST_PREFIX}-${RUST_RUSTFMT_TARBALL_NAME}.tar.xz
+"
+
+# The newlib src tarball was manually packed from a checkout of
+# https://sourceware.org/git/newlib-cygwin.git at ${NEWLIB_SHA}, using
+# ${FILESDIR}/pack_git_tarball.py.
+NEWLIB_SHA="1debd4d635c2"
+NEWLIB_SRC_TARBALL_NAME="newlib-${NEWLIB_SHA}-src"
+SRC_URI+=" https://commondatastorage.googleapis.com/chromeos-localmirror/distfiles/${NEWLIB_SRC_TARBALL_NAME}.tar.xz"
+
+SRC_ROOT="${WORKDIR}/${P}/src"
+INSTALL_ROOT="${WORKDIR}/${P}/install"
+INSTALL_PREFIX="opt/${PN}"
+
+# N.B., this toolchain is built entirely independently of the host's Rust
+# toolchain, so no dev-lang/rust dependency is needed.
+DEPEND="sys-libs/zlib
+	>=sys-libs/ncurses-5.9-r3
+	sys-devel/binutils"
+BDEPEND="${PYTHON_DEPS}
+	dev-lang/perl
+	sys-devel/gnuconfig
+	$(python_gen_any_dep 'dev-python/sphinx[${PYTHON_USEDEP}]')
+	>=dev-libs/libxml2-2.9.6
+	>=dev-lang/perl-5.0"
+
+pkg_setup() {
+	python-any-r1_pkg_setup
+}
+
+src_unpack() {
+	default
+
+	mkdir -p "${SRC_ROOT}" || die
+
+	mv "${WORKDIR}/${LLVM_SRC_TARBALL_NAME}" "${SRC_ROOT}/llvm" || die
+	mv "${WORKDIR}/${NEWLIB_SRC_TARBALL_NAME}" "${SRC_ROOT}/newlib" || die
+
+	mv "${WORKDIR}/${RUST_SRC_TARBALL_NAME}" "${SRC_ROOT}/rustc" || die
+	cp -r \
+		"${WORKDIR}/${RUST_STD_TARBALL_NAME}/rust-std-${BOOTSTRAP_HOST_TRIPLE}/lib/rustlib/${BOOTSTRAP_HOST_TRIPLE}" \
+		"${WORKDIR}/${RUST_STAGE0_TARBALL_NAME}/rustc/lib/rustlib" \
+		|| die
+}
+
+src_prepare() {
+	cd "${SRC_ROOT}/llvm" || die
+	eapply "${FILESDIR}/llvm11-10122020-soteria.patch"
+
+	cd "${SRC_ROOT}/rustc" || die
+
+	# Copy "unknown" vendor targets to create cros_sdk target triples applied later.
+	local spec_dir="compiler/rustc_target/src/spec"
+	sed -e 's|"unknown"|"pc"|g' "${spec_dir}/x86_64_unknown_linux_gnu.rs" \
+		> "${spec_dir}/x86_64_pc_linux_gnu.rs" \
+		|| die
+
+	eapply "${FILESDIR}/rust-add-cros-targets.patch"
+	sed -i 's|"stdc++"|"c++"|g' "compiler/rustc_llvm/build.rs" || die
+
+	cd "${SRC_ROOT}" || die
+	eapply_user
+}
+
+# src_configure is elided, since this package is actually building a few things
+# at once, and there are dependencies between these things. Ideally, each of
+# these would be their own ebuild, but we're trying to keep this as small and
+# self-contained as possible for the moment.
+
+src_compile() {
+	tc-export PKG_CONFIG
+
+	# In iterative development via `ebuild compile`, our clang toolchain
+	# might already be fully built. Don't rebuild it if that's the case.
+	if [[ ! -e "${INSTALL_ROOT}/bin/clang" ]]; then
+		"${FILESDIR}/build_clang_toolchain.py" \
+			--install-dir="${INSTALL_ROOT}" \
+			--llvm-dir="${SRC_ROOT}/llvm" \
+			--newlib-dir="${SRC_ROOT}/newlib" \
+			--work-dir="${SRC_ROOT}/llvm/build" \
+			|| die
+	fi
+
+	"${FILESDIR}/build_rust_toolchain.py" \
+		--install-dir="${INSTALL_ROOT}" \
+		--install-prefix="${ED}${INSTALL_PREFIX}" \
+		--rust-src="${SRC_ROOT}/rustc" \
+		--rv-clang-bin="${INSTALL_ROOT}/bin" \
+		--cargo="${WORKDIR}/${RUST_CARGO_TARBALL_NAME}/cargo/bin/cargo" \
+		--rustc="${WORKDIR}/${RUST_STAGE0_TARBALL_NAME}/rustc/bin/rustc" \
+		--rustfmt="${WORKDIR}/${RUST_RUSTFMT_TARBALL_NAME}/rustfmt-preview/bin/rustfmt" \
+		|| die
+}
+
+src_install() {
+	dodir "/${INSTALL_PREFIX}"
+	cp -a "${INSTALL_ROOT}"/* "${D}/${INSTALL_PREFIX}" || die
+}
