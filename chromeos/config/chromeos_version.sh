99366db86d4cb2416694ec483b86fd2eda825f1c
chromeos-ci-release <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>
2021-05-20 08:56:37 -0700

    Bump milestone after creating release branch release-R92-13982.B

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -45,7 +45,7 @@ else
 fi
 
 # Major version for Chrome.
-CHROME_BRANCH=92
+CHROME_BRANCH=93
 # Set CHROME values (Used for releases) to pass to chromeos-chrome-bin ebuild
 # URL to chrome archive
 CHROME_BASE=
