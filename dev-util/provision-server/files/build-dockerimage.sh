fa54a963329cf7bcb0cbf9e98f8995b0aa1e1037
C Shapiro <shapiroc@chromium.org>
2021-06-08 14:13:21 -0500

    Allow chroot to be passed for docker build
    This allows the script to be run locally or via CI with an alternate
    chroot path.
    
    BUG=None
    TEST=self
    
    Change-Id: Ibe18636ec2e610d9093835eb87dfc61d2eda1f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2947722
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>
    Reviewed-by: Mike Nichols <mikenichols@chromium.org>

--- dev-util/provision-server/files/build-dockerimage.sh
+++ dev-util/provision-server/files/build-dockerimage.sh
@@ -3,9 +3,23 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-set -ex
+set -e
 readonly script_dir="$(dirname "$(realpath -e "${BASH_SOURCE[0]}")")"
 
-build_context="${script_dir}/../../../../../../chroot/usr/bin"
+readonly chroot_arg="$1"
+readonly chroot_default="${script_dir}/../../../../../../chroot"
+
+chroot="${chroot_arg}"
+if [ -z "${chroot}" ]; then
+  echo "No chroot specified, so defaulting to: ${chroot_default}"
+  chroot="${chroot_default}"
+fi
+
+if [ ! -d "${chroot}" ]; then
+  echo "chroot path does not exist: ${chroot}"
+  exit 1
+fi
+
+build_context="${chroot}/usr/bin"
 
 sudo docker build -f "${script_dir}/Dockerfile" "${build_context}"
