72340758e511db23f504d398b3d455dc1ffc5448
Ilja H. Friedel <ihf@chromium.org>
2021-08-29 17:42:26 -0700

    Reland "media-gfx/deqp uprev to vulkan-cts-1.2.7.1"
    This reverts commit 5da279100664dd1166a94b929641435c034af419.
    
    * This fixes the script by pointing all source locations from Android to
      Khronos.
    * In addition we fix up the installed paths to the VK caselists.
    * Unfortunately the current 0.8.2 deqp-runner does not understand these
      caselists. So cat them together for now as a workaround.
    
    BUG=b:197031816
    TEST=hatch-borealis
    graphics_parallel_dEQP.vk   FAIL: Failed 258 tests:
    dEQP-VK.query_pool.statistics_query.fragment_shader_invocations.primary.32bits_cmdcopyquerypoolresults_line_strip,Fail,
    ..., dEQP-VK.subgroups.vote.ray_tracing.subgroupallequal_vec4,Crash (see failures.csv)
    
    Cq-Depend: chromium:3123649
    Change-Id: I5e168578cd8b8acd90321057fde8493b1312e543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/3123155
    Tested-by: Ilja H. Friedel <ihf@chromium.org>
    Auto-Submit: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Chad Versace <chadversary@chromium.org>

--- media-gfx/deqp/update.sh
+++ media-gfx/deqp/update.sh
@@ -6,13 +6,19 @@
 
 deqp_repo=$1
 
-if [ -z "$deqp_repo" ]; then
-    echo usage: $0 DEQP_REPO
+if [ -z "${deqp_repo}" ]; then
+    echo "usage:"
+    echo "* first obtain local copy of dEQP repo, for instance"
+    echo "  git clone https://github.com/KhronosGroup/VK-GL-CTS"
+    echo "* then checkout desired branch/tag/commit in VK-GL-CTS, for instance origin/vulkan-cts-1.2.7"
+    echo "* finally run this script inside the chroot"
+    echo "  . update.sh VK-GL-CTS"
     exit 1
 fi
 
 declare -A revisions
 declare -A git_repos=(
+    [deqp]=https://github.com/KhronosGroup/VK-GL-CTS/archive
     [SPIRV-Headers]=https://github.com/KhronosGroup/SPIRV-Headers/archive
     [SPIRV-Tools]=https://github.com/KhronosGroup/SPIRV-Tools/archive
     [glslang]=https://github.com/KhronosGroup/glslang/archive
@@ -30,10 +36,8 @@ for module in "${!git_repos[@]}"; do
     revisions[$module]=$revision
 done
 
-# Add the deqp repo and revision manually here, then do the transfers
-# and ebuild update.
-git_repos[deqp]=https://android.googlesource.com/platform/external/deqp/+archive
-revisions[deqp]=$(git -C $deqp_repo show-ref -s --head ^HEAD)
+# Do the transfers and ebuild update.
+revisions[deqp]=$(git -C "${deqp_repo}" show-ref -s --head ^HEAD)
 
 sed_cmd="${sed_cmd}s/MY_DEQP_COMMIT='.*'/MY_DEQP_COMMIT='${revisions[deqp]}'/;"
 
