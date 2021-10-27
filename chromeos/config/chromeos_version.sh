21b2b34f194cb0834b055efedc299122111ca4ab
Paul Fagerburg <pfagerburg@google.com>
2021-10-27 15:47:18 -0600

    Bump CHROMEOS_BUILD number for source branch main after creating branch firmware-chameleon-14280.B

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  CHROMEOS_BUILD=14311
+  CHROMEOS_BUILD=14312
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
