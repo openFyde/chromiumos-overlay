7b9dc673d33594c4c97588fe6c6c3358a9f6a7a4
Yong Zhi <yong.zhi@intel.com>
2021-12-03 18:38:34 -0600

    bluez: enable BT audio offload for some boards
    BUG=b:183594508
    TEST=manually cp the start_bluetoothd.sh to DUT at /usr/bin and
    main.conf at /etc/bluetooth
         1. restart bluetoothd
         2. check /var/log/messages for bluetoothd log
    
    Cq-Depend: chrome-internal:4312016
    Change-Id: Ie91be35001e13dc1622953bcce5ac69ba9faf76b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/3314820
    Tested-by: Michael Sun <michaelfsun@google.com>
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Commit-Queue: Michael Sun <michaelfsun@google.com>

--- net-wireless/bluez/files/start_bluetoothd.sh
+++ net-wireless/bluez/files/start_bluetoothd.sh
@@ -10,6 +10,12 @@
 
 bluetooth_dir="/etc/bluetooth"
 conf_file="${bluetooth_dir}/main.conf"
+experimental="d4992530-b9ec-469f-ab01-6c481c47da1c,671b10b5-42c0-4696-9227-eb28d1b049d6,330859bc-7506-492d-9370-9a6f0614037f"
+
+bt_offload="$(cros_config /bluetooth/flags enable-bluetooth-offload)"
+if [ "$bt_offload" = "true" ]; then
+    experimental="${experimental},a6695ace-ee7f-4fb9-881a-5fac66c629af"
+fi
 
 # Make a copy of main.conf to /var to make it editable
 var_conf_file="/var/lib/bluetooth/main.conf"
@@ -27,4 +33,4 @@ config_file_param="--configfile=${var_conf_file}"
 exec /sbin/minijail0 -u bluetooth -g bluetooth -G \
   -c 3500 -n -- \
   /usr/libexec/bluetooth/bluetoothd "${BLUETOOTH_DAEMON_OPTION}" --nodetach \
-  "${config_file_param}"
+  "${config_file_param}" -E "${experimental}"
