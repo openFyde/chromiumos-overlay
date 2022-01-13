db5d0f3ce515ce31b2c2786b67fd86afbb8af546
Jack Rosenthal <jrosenth@chromium.org>
2022-01-12 23:50:01 -0700

    chromeos-config: Delete empty_config.c
    Meant to delete this in CL:3384081 since it's not installed anymore.
    
    BUG=none
    TEST=CQ
    
    Change-Id: Ida26a7cc20f9d3e0f5fa1735c88132d30e39ca9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/3385836
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Auto-Submit: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- chromeos-base/chromeos-config/files/empty_config.c
+++
@@ -1,8 +0,0 @@
-#include "lib/cros_config_struct.h"
-
-static struct config_map all_configs[] = {};
-
-const struct config_map *cros_config_get_config_map(int *num_entries) {
-  *num_entries = 0;
-  return &all_configs[0];
-}
