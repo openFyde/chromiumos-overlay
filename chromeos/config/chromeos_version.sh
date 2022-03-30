1d3b63ab49220d8413dbf4986944f1bb8491b3bc
chromeos-ci-release <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>
2022-03-30 11:02:45 -0700

    Increment to version R102-14652.0.0
    Generated by release-main-orchestrator.
    
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8818245637353716305
    Cr-Automation-Id: cros_version/bump_version
    Change-Id: I4b22f414975b39f41bbd916dc3e036aee61ded02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/3561083
    Tested-by: chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>
    Bot-Commit: chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  CHROMEOS_BUILD=14651
+  CHROMEOS_BUILD=14652
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
