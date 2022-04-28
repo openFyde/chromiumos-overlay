389a0d1ce52922d9afde66654a180558c277c84b
Brian Geffon <bgeffon@chromium.org>
2022-04-28 10:40:17 -0400

    swap-init: Drop per-user and per-board overrides.
    Per-device (user) overrides have been disabled for quite
    some time now, see get_target_value().
    
    Additionally, we haven't use a per-board override since Caroline.
    Since we haven't had a clear regular need for per-board specific
    overrides let's drop that code too.
    
    BUG=b/230658588
    TEST=Betty VM
    
    Change-Id: Ifa485f4d4662ed005015f91d6a3b2946f62c4196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/3613753
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Brian Geffon <bgeffon@chromium.org>

--- chromeos-base/swap-init/files/init/swap.sh
+++ chromeos-base/swap-init/files/init/swap.sh
@@ -3,16 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Set margin for low-memory notifier (for tab discarder)
-# Configure and start swap if SWAP_ENABLE_FILE exists.
-# SWAP_ENABLE_FILE may optionally contain the uncompressed swap size (in Mb).
-# Otherwise it is set to 1.5 times total RAM.
-
-PER_BOARD_OVERRIDE_DIR=/etc/swap
-PER_DEVICE_OVERRIDE_DIR=/var/lib/swap
-
-SWAP_SIZE_BOARD_OVERRIDE_FILE="${PER_BOARD_OVERRIDE_DIR}/swap_size_mb"
-SWAP_ENABLE_FILE="${PER_DEVICE_OVERRIDE_DIR}/swap_enabled"
+SWAP_ENABLE_FILE="/var/lib/swap/swap_enabled"
 ZRAM_BACKING_DEV="/sys/block/zram0/backing_dev"
 ZRAM_WRITEBACK_NAME="zram-writeback"
 ZRAM_INTEGRITY_NAME="zram-integrity"
@@ -38,22 +29,12 @@ MAX_ZRAM_WRITEBACK_SIZE_MB=6144
 # Don't allow a size less than 128MB to be configured.
 MIN_ZRAM_WRITEBACK_SIZE_MB=128
 
-MARGIN_MAX=20000  # MiB
-MARGIN_CONVERSION=1
-MARGIN_OVERRIDE_FILE="${PER_DEVICE_OVERRIDE_DIR}/lowmem_margin"
-MARGIN_BOARD_OVERRIDE_FILE="${PER_BOARD_OVERRIDE_DIR}/lowmem_margin"
 MARGIN_SPECIAL_FILE="/sys/kernel/mm/chromeos-low_mem/margin"
-
 margin_default_generator() {
   default_low_memory_margin "$(get_mem_total)"  # MiB
 }
 
-MIN_FILELIST_MAX=1000  # MiB
-MIN_FILELIST_CONVERSION=1024
-MIN_FILELIST_OVERRIDE_FILE="${PER_DEVICE_OVERRIDE_DIR}/min_filelist_kbytes"
-MIN_FILELIST_BOARD_OVERRIDE_FILE="${PER_BOARD_OVERRIDE_DIR}/min_filelist_kbytes"
 MIN_FILELIST_SPECIAL_FILE="/proc/sys/vm/min_filelist_kbytes"
-
 min_filelist_default_generator() {
   # Check if ARC++ is running.  But don't check if it's not installed.
   if grep -q CHROMEOS_ARC_VERSION /etc/lsb-release && \
@@ -65,22 +46,12 @@ min_filelist_default_generator() {
   fi
 }
 
-EXTRA_FREE_MAX=20000  # MiB
-EXTRA_FREE_CONVERSION=1024
-EXTRA_FREE_OVERRIDE_FILE="${PER_DEVICE_OVERRIDE_DIR}/extra_free_kbytes"
-EXTRA_FREE_BOARD_OVERRIDE_FILE="${PER_BOARD_OVERRIDE_DIR}/extra_free_kbytes"
 EXTRA_FREE_SPECIAL_FILE="/proc/sys/vm/extra_free_kbytes"
-
 extra_free_default_generator() {
   echo 0
 }
 
-RAM_VS_SWAP_WEIGHT_MAX=256
-RAM_VS_SWAP_WEIGHT_CONVERSION=1
-RAM_VS_SWAP_WEIGHT_OVERRIDE_FILE="${PER_DEVICE_OVERRIDE_DIR}/ram_vs_swap_weight"
-RAM_VS_SWAP_WEIGHT_BOARD_OVERRIDE_FILE="${PER_BOARD_OVERRIDE_DIR}/ram_vs_swap_weight"
 RAM_VS_SWAP_WEIGHT_SPECIAL_FILE="/sys/kernel/mm/chromeos-low_mem/ram_vs_swap_weight"
-
 ram_vs_swap_weight_default_generator() {
   # Historically we've found that zram compresses approximately at this ratio
   # which is why this is the current default.
@@ -128,31 +99,12 @@ default_low_memory_margin() {
 
 # Gets the target value of a kernel memory manager parameter, whose name is
 # passed in $1.
-#
-# Each parameter <P> has a default value, computed by <P>_default_generator.
-# The default value can be overridden by a board-specific value contained
-# in <P>_BOARD_OVERRIDE_FILE, or (with higher priority) a device-specific file,
-# <P>_OVERRIDE_FILE which a user may have set, typically for the purpose of
-# experimentation.
 get_target_value() {
   local PARAM="$(echo "$1" | tr '[a-z]' '[A-Z]')"
   local value
-  local override_file="$(expand_var "${PARAM}_OVERRIDE_FILE")"
-  local board_override_file="$(expand_var "${PARAM}_BOARD_OVERRIDE_FILE")"
   local default_generator="$1"_default_generator
 
-  if [ -e "${override_file}" ]; then
-    rm -f "${override_file}" 2> /dev/null
-  fi
-
-  # We no longer allow per-user overrides because they were causing issues
-  # where users were unaware of kernel changes requiring new parameters.
-  # Additionally, user overrides prevent finch based experimentation.
-  if [ -e "${board_override_file}" ]; then
-    value=$(cat "${board_override_file}")
-  else
-    value=$(${default_generator})
-  fi
+  value=$(${default_generator})
   echo "${value}"
 }
 
@@ -221,10 +173,6 @@ start() {
   local requested_size_mb size_kb
   # For security, only read first few bytes of SWAP_ENABLE_FILE.
   requested_size_mb="$(head -c 5 "${SWAP_ENABLE_FILE}")" || :
-  # If SWAP_ENABLE_FILE does not exist or is empty, try the board override.
-  if [ -z "${requested_size_mb}" ]; then
-    requested_size_mb=$(cat "${SWAP_SIZE_BOARD_OVERRIDE_FILE}") || :
-  fi
   # If still empty, compute swap based on RAM size.
   if [ -z "${requested_size_mb}" ]; then
     # Default multiplier for zram size. (Shell math is integer only.)
@@ -517,49 +465,6 @@ enable_zram_writeback() {
   echo "Enabled writeback with size ${writeback_size_mb}MB"
 }
 
-set_parameter() {
-  local param="$1"
-  local value="$2"
-  case "${param}" in
-  margin|min_filelist|extra_free|ram_vs_swap_weight)
-    # We're good.
-    ;;
-  *)
-    echo "invalid parameter ${param}" >&2
-    exit 1
-    ;;
-  esac
-  local PARAM="$(echo "${param}" | tr '[:lower:]' '[:upper:]')"
-  local max="$(expand_var "${PARAM}_MAX")"
-  local special_file="$(expand_var "${PARAM}_SPECIAL_FILE")"
-  local conversion="$(expand_var "${PARAM}_CONVERSION")"
-  local default_generator=${param}_default_generator
-
-  # Don't try to set the parameter if the kernel doesn't support it.
-  if [ ! -e "${special_file}" ]; then
-    return 0
-  fi
-
-  if [ "${value}" -gt "${max}" ]; then
-    echo "${JOB}: invalid set ${param} to ${value} (MiB), max is ${max}" >&2
-    exit 1
-  fi
-
-  # We no longer allow per-user overrides because they were causing issues
-  # where users were unaware of kernel changes requiring new parameters.
-  # Additionally, user overrides prevent finch based experimentation.
-  local system_value
-  if [ "${value}" = "-1" ]; then
-    system_value=$(get_target_value "${param}")
-  else
-    # User units (always MiB) may differ from system units (sometimes KiB).
-    system_value=$(( value * conversion ))
-  fi
-  echo "${system_value}" > "${special_file}"
-  value=$(( system_value / conversion ))
-  logger "changed ${param} to ${value} MiB"
-}
-
 usage() {
   cat <<EOF
 Usage: $0  command <params>
@@ -570,15 +475,11 @@ Usage: $0  command <params>
            status
            enable <size>
            disable
-           set_parameter <margin|min_filelist|extra_free|ram_vs_swap_weight> <value>
-           get_target_value <margin|min_filelist|extra_free|ram_vs_swap_weight> <value>
+           get_target_value <margin|min_filelist|extra_free|ram_vs_swap_weight>
            enable_zram_writeback <size_mb>
 
-Start or stop the use of the compressed swap file, or persistently set
-various memory manager tunable parameters, or get their chosen values.
-
-A value of -1 for "enable" or "set_parameter <p>" results in removing any local
-persistent setting of the parameter, thus reverting to its factory default.
+Start or stop the use of the compressed swap file and get memory manager
+tunable values.
 
 The start phase is normally invoked by init during boot, but we never run the
 stop phase when shutting down (since there's no point).  The stop phase is used
@@ -610,11 +511,6 @@ main() {
     ;;
   enable|get_target_value|enable_zram_writeback)
     if [ $# -ne 1 ]; then
-      usage 1
-    fi
-    ;;
-  set_parameter)
-    if [ $# -ne 2 ]; then
       usage 1
     fi
     ;;
