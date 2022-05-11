6d65802ad120d968389402a208dedabccf8ac5f5
Kenny Pan <kennypan@google.com>
2022-05-11 01:52:09 +0000

    Bump CHROMEOS_BUILD number for source branch main after creating branch factory-14778.B

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  CHROMEOS_BUILD=14804
+  CHROMEOS_BUILD=14805
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
