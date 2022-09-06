58b415fa3e2b71d28250f2c3e2c20ef7ef4f8b4a
Madeleine Hardt <hardtmad@google.com>
2022-09-06 23:59:15 +0000

    Bump CHROMEOS_BUILD number for source branch main after creating branch firmware-14947.B

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  CHROMEOS_BUILD=15096
+  CHROMEOS_BUILD=15097
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
