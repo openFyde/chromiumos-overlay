658d0e22bab6bed89e9d869587ad53ab29eb0bd7
chromeos-ci-prod <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
2022-09-25 05:07:42 -0700

    Automatic: master-release - Updating to a new version number from 15140.0.0
    Change-Id: I2b69e5dec78fba2835704098e885456c2c3df979

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -14,7 +14,7 @@
 #############################################################################
 # Release Build number.
 # Increment by 1 for every release build.
-CHROMEOS_BUILD=15140
+CHROMEOS_BUILD=15141
 
 # Release Branch number.
 # Increment by 1 for every release build on a branch.
