6c7bc1ca5d93018d5cb04ea93ae8781040d36aa7
chromeos-ci-release <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>
2022-09-30 16:47:27 -0700

    Increment to version R108-15152.0.0
    Generated by release-main-orchestrator.
    
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8801555813365693697
    Cr-Automation-Id: cros_version/bump_version
    Change-Id: Ifb4f6a11e375891d1be2158fed4d9148c963d81b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/3930968
    Bot-Commit: chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>
    Tested-by: chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -14,7 +14,7 @@
 #############################################################################
 # Release Build number.
 # Increment by 1 for every release build.
-CHROMEOS_BUILD=15151
+CHROMEOS_BUILD=15152
 
 # Release Branch number.
 # Increment by 1 for every release build on a branch.
