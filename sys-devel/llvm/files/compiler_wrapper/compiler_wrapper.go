1a8d603d4e69f1c78bf9de625a112371956fb200
Ryan Beltran <ryanbeltran@chromium.org>
2022-11-04 00:02:30 +0000

    llvm: bundle compiler-wrapper support for IWYU
    This CL is made by bundling the compiler wrapper with changes from
    crrev.com/c/3997486
    
    This makes it possible to invoke IWYU with the compiler_wrapper.
    
    BUG=b:237320348
    TEST=bundle.py
    
    Change-Id: I415b042cf92c644027f12b2570a966add3ce029a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/4004530
    Tested-by: Ryan Beltran <ryanbeltran@chromium.org>
    Reviewed-by: Manoj Gupta <manojgupta@chromium.org>

--- sys-devel/llvm/files/compiler_wrapper/compiler_wrapper.go
+++ sys-devel/llvm/files/compiler_wrapper/compiler_wrapper.go
@@ -151,6 +151,7 @@ func callCompilerInternal(env env, cfg *config, inputCmd *command) (exitCode int
 		}
 	} else {
 		cSrcFile, tidyFlags, tidyMode := processClangTidyFlags(mainBuilder)
+		cSrcFile, iwyuFlags, iwyuMode := processIWYUFlags(mainBuilder)
 		if mainBuilder.target.compilerType == clangType {
 			err := prepareClangCommand(mainBuilder)
 			if err != nil {
@@ -176,6 +177,20 @@ func callCompilerInternal(env env, cfg *config, inputCmd *command) (exitCode int
 					return 0, err
 				}
 			}
+
+			if iwyuMode != iwyuModeNone {
+				if iwyuMode == iwyuModeError {
+					panic(fmt.Sprintf("Unknown IWYU mode"))
+				}
+
+				allowCCache = false
+				clangCmdWithoutRemoteBuildAndCCache := mainBuilder.build()
+				err := runIWYU(env, clangCmdWithoutRemoteBuildAndCCache, cSrcFile, iwyuFlags)
+				if err != nil {
+					return 0, err
+				}
+			}
+
 			if remoteBuildUsed, err = processRemoteBuildAndCCacheFlags(allowCCache, mainBuilder); err != nil {
 				return 0, err
 			}
