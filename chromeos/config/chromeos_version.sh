34b413103bacc7d4156994148410b37e316c8a1c
chromeos-ci-release <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>
2023-01-03 16:00:34 -0800

    Bump CHROMEOS_BUILD number for source branch main after creating branch stabilize-15301.B

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -14,7 +14,7 @@
 #############################################################################
 # Release Build number.
 # Increment by 1 for every release build.
-CHROMEOS_BUILD=15301
+CHROMEOS_BUILD=15302
 
 # Release Branch number.
 # Increment by 1 for every release build on a branch.
