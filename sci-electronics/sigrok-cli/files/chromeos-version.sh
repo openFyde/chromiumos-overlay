f27d5225b386893197a11bf4e9c867c23d73f2de
Mike Frysinger <vapier@chromium.org>
2023-02-14 09:07:42 -0500

    sigrok: drop unused packages
    Nothing has ever depended on these, and they haven't been updated in
    years.  The distro versions are most likely newer at this point.
    
    BUG=b:187789240
    TEST=CQ passes
    
    Change-Id: Iaa76ec11bd4ba87aa1582c4ad8d98ef47d9ae334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/4246889
    Auto-Submit: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- sci-electronics/sigrok-cli/files/chromeos-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright 2014 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-exec gawk '
-match($0, /AC_INIT\(\[sigrok-cli\], \[([0-9.]+)\],/, res) { version = res[1] }
-END { print version }' "$1/configure.ac"
